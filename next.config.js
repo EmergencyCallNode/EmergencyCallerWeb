8dc7added7f2c5eae8f7d1c626db083be0c8679b
ADev999 <ashleyssharper@gmail.com>
2024-03-19 14:49:23 -0400

    update env

--- next.config.js
+++ next.config.js
@@ -5,3 +5,10 @@
   }
 
   module.exports = nextConfig
+//   module.exports = {
+//     reactStrictMode: true,
+//     webpackDevMiddleware: (config) => {
+//         config.watchOptions.poll = 300;
+//         return config;
+//     },
+// };
